7809c374a4c1b6e792848e6f57cb67951868e8c3
Van Nguyen <VanNgnRan@gmail.com>
2024-01-05 15:43:01 +0700

    config database

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config()
 
 async function connect() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/TradeGoods");
+        await mongoose.connect(process.env.MONGO_DB);
 
         console.log("Connect to database successfully!");
     } catch (error) {
