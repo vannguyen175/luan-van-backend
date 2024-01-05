628dee98196e7b247f3872586fed90babcd59c0c
Van Nguyen <VanNgnRan@gmail.com>
2024-01-05 15:44:55 +0700

    UserService

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,5 +1,6 @@
 const User = require("../models/UserModel");
-const bcrypt = require("bcrypt");  //ma hoa mat khau
+const bcrypt = require("bcrypt"); //ma hoa mat khau
+const { genneralAccessToken } = require("./JwtService");
 
 const createUser = (newUser) => {
     return new Promise(async (resolve, reject) => {
@@ -14,14 +15,13 @@ const createUser = (newUser) => {
                     message: "Email already exists",
                 });
             }
-     
-            const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds)
-            
+
+            const hash = bcrypt.hashSync(password, 10);
+
             const createUser = await User.create({
                 name,
                 email,
-                password,
-                confirmPassword,
+                password: hash,
                 phone,
             });
             if (createUser) {
@@ -37,4 +37,109 @@ const createUser = (newUser) => {
     });
 };
 
-module.exports = { createUser };
+const loginUser = (loginUser) => {
+    return new Promise(async (resolve, reject) => {
+        const { email, password } = loginUser;
+        try {
+            const checkUser = await User.findOne({ email: email });
+            if (checkUser === null) {
+                resolve({
+                    status: "OK",
+                    message:
+                        "The email address or password is incorrect. Please retry...",
+                });
+            }
+
+            //sau khi ktra login hop le
+            const access_token = await genneralAccessToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin,
+            });
+
+            resolve({
+                status: "OK",
+                message: "SUCCESS",
+                access_token,
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+const updateUser = (userID, data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkUser = await User.findOne({ _id: userID });
+            if (checkUser === null) {
+                resolve({
+                    status: "OK",
+                    message: "User is not exists",
+                });
+            }
+            const updateUser = await User.findByIdAndUpdate(userID, data, {
+                new: true,
+            });
+            resolve({
+                status: "OK",
+                message: "SUCCESS",
+                data: updateUser,
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+const deleteUser = (userID) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkUser = await User.findOne({ _id: userID });
+            if (checkUser === null) {
+                resolve({
+                    status: "OK",
+                    message: "User is not exists",
+                });
+            }
+            await User.findByIdAndDelete(userID);
+            resolve({
+                status: "OK",
+                message: "Delete user successfully",
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+const getAllUsers = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const result = await User.find();
+            resolve({
+                status: "OK",
+                message: "SUCCESS",
+                data: result,
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+const detailUser = (userID) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const result = await User.findById(userID);
+            resolve({
+                status: "OK",
+                message: "SUCCESS",
+                result,
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+module.exports = { createUser, loginUser, updateUser, deleteUser, getAllUsers, detailUser };
